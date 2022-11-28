8d1d7127f4781a727ddba90828e6ed7b937b7a6e
Diego Soto <juandisoca@gmail.com>
2022-11-28 16:52:23 -0600

    About section -> Motion text added

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,5 +1,4 @@
 'use client';
-'use client';
 
 import { motion } from 'framer-motion';
 import { TypingText } from '../components';
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,7 +1,26 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { TypingText, ExploreCard, TitleText } from '../components';
+import { staggerContainer } from '../utils/motion';
+import styles from '../styles';
+
+import { exploreWorlds } from '../constants';
+
 const Explore = () => (
-  <section>
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.2 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+
+    >
+      <TypingText title="| The world" textStyles="text-center" />
+      <TitleText title={<>Chose the world you want <br className="md:block hidden" /> to explore </>} textStyles="text-center" />
+    </motion.div>
     Explore section
   </section>
 );
