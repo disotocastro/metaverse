b5f8611e20cde0681e2809ad1b0dd43844051188
Diego Soto <juandisoca@gmail.com>
2022-11-29 15:28:01 -0600

    startsetps completed

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 import { StartSteps, TitleText, TypingText } from '../components';
-import { startingFeature } from '../constants';
+import { startingFeatures } from '../constants';
 
 const GetStarted = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -21,6 +21,25 @@ const GetStarted = () => (
       >
         <img src="/get-started.png" alt="get-started " className="w-[90%] h-[90%] object-contain" />
       </motion.div>
+
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| How Metaversus works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps
+              key={feature}
+              number={index + 1}
+              text={feature}
+            />
+
+          ))}
+        </div>
+      </motion.div>
+
     </motion.div>
     Get Started Section
   </section>
